402b0fc86c589461759544dfb679a1383812eaff
Bjørn Kristian Larsen <bjornkl@met.no>
2022-06-27 20:50:18 +0200

    initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,8 @@
+import Button from "@mui/material/Button";
+import React from "react";
+
+const App: React.FC = () => {
+  return <Button variant="contained">Click Me</Button>;
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const prod = process.env.NODE_ENV === "production";
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: prod ? "production" : "development",
+  entry: "./src/index.tsx",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+    alias: {
+      "@mui/styled-engine": "@mui/styled-engine-sc",
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ["ts-loader"],
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+  devtool: prod ? undefined : "source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "index.html"),
+    }),
+  ],
+};
