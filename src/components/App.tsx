00a6768f1a824eb33b4a15931db17909f061eec2
Bjørn Kristian Larsen <bjornkl@met.no>
2022-06-28 12:13:04 +0200

    added example content

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,39 @@
-import Button from '@mui/material/Button/Button';
-import React from 'react';
+import { ThemeProvider } from '@emotion/react';
+import { Box, createTheme, SimplePaletteColorOptions } from '@mui/material';
+import React, { useState } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import {
+  createMetTheme,
+  paletteAsString,
+  paletteMap,
+} from '../utils/metMuiThemes';
+import Footer from './Footer';
+import Header from './Header';
+import Home from './Home';
 
 const App: React.FC = () => {
-  return <Button variant="contained">Click Me</Button>;
+  return (
+    <ThemeProvider
+      theme={createTheme({
+        palette: {
+          primary: paletteMap.get('teal_palette'),
+          secondary: paletteMap.get('black_palette'),
+        },
+      })}
+    >
+      <Box
+        component="div"
+        sx={{
+          height: '100%',
+          backgroundImage: 'url(/images/waves.png)',
+        }}
+      >
+        <Header />
+        <Home />
+        <Footer />
+      </Box>
+    </ThemeProvider>
+  );
 };
 
 export default App;
