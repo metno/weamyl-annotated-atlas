5187c078e6c7d8f1b7c7b6fdaa3273ef538f1feb
Hans Christian Nenseth <hanscn@met.no>
2023-05-22 10:56:54 +0200

    added userloggedin

---
+++ src/components/UserInfo.tsx
@@ -0,0 +1,20 @@
+import { useAuth } from 'react-oidc-context';
+import React from 'react';
+
+const UserInfo: React.FC = () => {
+  const auth = useAuth();
+
+  if (auth.isAuthenticated) {
+    return (
+      <ul>
+        <li>Name: {auth.user?.profile.name}</li>
+        <li>Username: {auth.user?.profile.preferred_username}</li>
+        <li>Email: {auth.user?.profile.email}</li>
+      </ul>
+    );
+  }
+
+  return <div>Not logged in</div>;
+};
+
+export default UserInfo;
