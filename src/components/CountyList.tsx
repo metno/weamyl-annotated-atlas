842509b9fb1389258c2e7800fb4267f42f40a9ec
Hans Christian Nenseth <hanscn@met.no>
2024-09-24 13:10:51 +0200

    vip counties

--- src/components/CountyList.tsx
+++ src/components/CountyList.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import CreatableSelect from 'react-select/creatable';
 import databaseFunctions from '../utils/databaseFunctions';
-import { Box, Typography } from '@mui/material';
+import { Box, responsiveFontSizes, Typography } from '@mui/material';
 import Select from 'react-select';
 
 type Props = {
@@ -17,14 +17,14 @@ const CountyName: React.FC<Props> = ({ searchObject, setSearchObject }) => {
     .getCountyList()
       .then((response) => {
         setCounty(response.data);
-        //console.log(response.data);
+        console.log(response.data);
     });
   }, []);
 
   const optionList = [];
   for (let i = 0; i < county.length; i += 1) {
     optionList[i] = {
-      value: county[i][0],
+      value: county[i][1],
       label: county[i][0],
     };
   }
@@ -36,7 +36,16 @@ const CountyName: React.FC<Props> = ({ searchObject, setSearchObject }) => {
         value: '',
       };
     }
-    const phenomSearch = { ...searchObject, countyName: option.value };
+    const selectedCountyId = option.value;
+    const selectedCountyName = option.label;
+
+    databaseFunctions
+    .getlowresCountyPolygon(selectedCountyId)
+      .then((response) =>{
+        console.log(response.data)
+      })
+
+    const phenomSearch = { ...searchObject, countyName: selectedCountyName };
     setSearchObject(phenomSearch);
     console.log(phenomSearch);
   };
