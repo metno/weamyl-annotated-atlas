5c1548483094371691a94f20dd4bbf291a78a803
Hans Christian Nenseth <hanscn@met.no>
2026-09-17 15:57:49 +0200

    redirectUri

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import { AuthProvider } from 'react-oidc-context';
 // TODO: Update with relevant info when project approved
 
 const applicationRoot = `${window.location.origin}/`;
-const redirectUri = `${applicationRoot}oidc-callback`;
+const redirectUri = `${applicationRoot}callback`;
 
 const oidcConfig = {
   authority: 'https://login.met.no/auth/realms/Internal/',
